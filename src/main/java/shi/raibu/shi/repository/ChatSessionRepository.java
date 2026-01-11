e41a1d5a73f6f88bdf398f6255c4202ac7f57df5
Mathieu <hei.tafita.2@gmail.com>
2026-01-11 12:58:39 +0300

    feat: impl ChatSessionRepository::findUser1IdOr/AndUser2Id

---
+++ src/main/java/shi/raibu/shi/repository/ChatSessionRepository.java
@@ -0,0 +1,16 @@
+package shi.raibu.shi.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import shi.raibu.shi.model.ChatSession;
+
+@Repository
+public interface ChatSessionRepository extends JpaRepository<ChatSession, String> {
+  List<ChatSession> findByUser1IdOrUser2Id(String user1Id, String user2Id);
+
+  Optional<ChatSession> findByUser1IdAndUser2IdAndStatus(
+      String user1Id, String user2Id, ChatSession.SessionStatus status);
+}
