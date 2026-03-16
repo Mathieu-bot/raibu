cadbd4f733ca898b95a6d976fc47d664bd3d86ae
Mathieu <hei.tafita.2@gmail.com>
2026-03-16 22:23:26 +0300

    feat(aop): enable AspectJ auto-proxy in main application class

--- src/main/java/shi/raibu/shi/PojaApplication.java
+++ src/main/java/shi/raibu/shi/PojaApplication.java
@@ -2,8 +2,10 @@ package shi.raibu.shi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @PojaGenerated
 public class PojaApplication {
 
